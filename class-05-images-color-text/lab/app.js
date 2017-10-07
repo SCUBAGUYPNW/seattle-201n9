37d6c1967c5e36ead9a88fd2340850bbcc9da7d7
Mitchel Hall <scubaphoto@gmail.com>
2017-10-07 15:45:42 -0700

    Question2 passed test

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -54,11 +54,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var sumOfThree = sum(sum(a,b)[0],c)[0];
+  var productOfThree = multiply(multiply(a,b)[0],c)[0];
+  var messageSum = a + ' and ' + b + ' and ' + c + ' sum to ' + sumOfThree + '.';
+  var messageProd = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productOfThree + '.';
+  return [sumOfThree, productOfThree, messageSum, messageProd];
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
